cff8dd0e796eb4142c1018aeda8668e841ac8a93
Patrick Hannan <patrick@hannan.com>
2020-10-02 16:09:40 -0700

    added middleware

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require("express");
+const fs = require("fs");
 const app = express();
 
 const PORT = process.env.PORT || 8080;
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 app.listen(PORT, () => {
     console.log(`Currently running on http://localhost:${PORT}`);
-})
+});
