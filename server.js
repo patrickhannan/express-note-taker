837612f5b8a6a27bd4a2498738c6989ccd903975
Patrick Hannan <patrick@hannan.com>
2020-10-02 16:16:49 -0700

    Added post route

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ const PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.post("/api/notes", (req, res) => {
+    
+  });
+
 app.listen(PORT, () => {
     console.log(`Currently running on http://localhost:${PORT}`);
 });
