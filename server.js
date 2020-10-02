e9bfa348f510d1f0c9a19195059e7c2a276d515b
Patrick Hannan <patrick@hannan.com>
2020-10-02 16:08:04 -0700

    Listening on the port

--- server.js
+++ server.js
@@ -2,3 +2,7 @@ const express = require("express");
 const app = express();
 
 const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+    console.log(`Currently running on http://localhost:${PORT}`);
+})
