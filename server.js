c80723705c892d48347332145211161f355abd33
Patrick Hannan <patrick@hannan.com>
2020-10-02 17:03:22 -0700

    added error and confirmation messages.

--- server.js
+++ server.js
@@ -10,13 +10,25 @@ app.use(express.json());
 app.post("/api/notes", (req, res) => {
     console.log(req.body)
     fs.readFile("./db/db.json", "utf-8", (err, data) => {
-        if(err) throw err;
+        if(err) {
+            console.log(err);
+            return res.json({
+                error: true,
+                data: null,
+                message: "Unable to retrieve notes.",
+            });
+        }
         console.log(data);
         const updatedData = JSON.parse(data);
         updatedData.push(req.body);
         console.log(updatedData);
         fs.writeFile("./db/db.json", JSON.stringify(receivedData), (err) => {
             if (err) throw err;
+            res.json({
+                error: false,
+                data: updatedData,
+                message: "Successfully added new note."
+            })
         });
     })
   });
