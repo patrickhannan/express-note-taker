99646343384d99aa885f27d3343c4a0bfe8e7061
Patrick Hannan <patrick@hannan.com>
2020-10-02 17:25:27 -0700

    Requires path.

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const fs = require("fs");
 const app = express();
-
+const path = require("path");
 const PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
