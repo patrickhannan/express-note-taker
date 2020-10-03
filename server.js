4372de5c4c080a4a50a227ac202cb5551aff18e3
Patrick Hannan <patrick@hannan.com>
2020-10-02 17:22:58 -0700

    Joins notes file.

--- server.js
+++ server.js
@@ -40,6 +40,10 @@ app.get("*", function (req, res) {
     res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+
 app.listen(PORT, () => {
     console.log(`Currently running on http://localhost:${PORT}`);
 });
